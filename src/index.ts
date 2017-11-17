62e3230ed9a28a8fdee4c9d0ed5a74c9e2ee2631
choui <zw454475525@sina.com>
2017-11-17 11:08:18 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,16 @@ import { router } from "./router";
 import { startCrawl } from "./task";
 import { Logger } from './util/logger';
 import { createConnection } from 'typeorm';
-import cors =  require("koa-cors");
+//import cors =  require("koa-cors");
 
 
+console.log('start app!');
 
 createConnection().then(async connection => {
     Logger.log('typeorm connected');
     startCrawl(connection);
     const app = new koa(); 
-    app.use(cors());
+    //app.use(cors());
     app.use(router.routes());
-    app.listen(3000);
+    app.listen(3003);
 }).catch(error => console.log(error)); 
