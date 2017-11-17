c89c673e3433d6c99f0c8d6b57958e1d125b4679
choui <zw454475525@sina.com>
2017-11-17 13:42:48 +0800

    delet node_module

--- src/dao/city.ts
+++ src/dao/city.ts
@@ -1,12 +1,12 @@
-import {getRepository} from "typeorm";
-import { City } from '../entity/city'; 
+import { getRepository } from "typeorm";
+import { City } from '../entity/city';
 import { Injectable } from 'container-ioc';
 
- @Injectable()
-export class cityDao{
-    
-    async getCitys():Promise<City[]>{
-        let result:City[] = await getRepository(City).createQueryBuilder('city').getMany();
+@Injectable()
+export class cityDao {
+
+    async getCitys(): Promise<City[]> { 
+        let result: City[] = await getRepository(City).createQueryBuilder('city').getMany(); 
         return result;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,18 @@ import { router } from "./router";
 import { startCrawl } from "./task";
 import { Logger } from './util/logger';
 import { createConnection } from 'typeorm';
-//import cors =  require("koa-cors");
+import cors =  require("koa-cors");
 
 
 console.log('start app!');
 
-createConnection().then(async connection => {
-    Logger.log('typeorm connected');
+createConnection().then(async connection => { 
+    console.log('connected app!');
     startCrawl(connection);
     const app = new koa(); 
-    //app.use(cors());
+    app.use(cors());
     app.use(router.routes());
     app.listen(3003);
 }).catch(error => console.log(error)); 
+
+
